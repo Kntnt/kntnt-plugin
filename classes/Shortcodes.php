8c604607075b2be78e49317d8e2a9d877a124bf4
Thomas Barregren <thomas@kntnt.com>
2021-05-09 21:51:32 +0200

    Added helper methods.

--- classes/Shortcodes.php
+++ classes/Shortcodes.php
@@ -6,8 +6,45 @@ namespace Kntnt\Plugin;
 
 trait Shortcodes {
 
+	public static function boolean( $value ): ?bool {
+		switch ( strtolower( $value ) ) {
+			case 0:
+			case 'no':
+			case 'false':
+				return false;
+			case 1:
+			case 'yes':
+			case 'true':
+				return true;
+			default:
+				return null;
+		}
+	}
+
+	// Removes unwanted leading or trailing <br />, <p> and </p> which are
+	// artifacts of wpautop() which runs before shortcodes.
+	public static function remove_wpautop_artifacts( $content ) {
+
+		if ( substr( $content, 0, 5 ) == "</p>\n" ) {
+			$content = substr( $content, 5 );
+		}
+		else if ( substr( $content, 0, 7 ) == "<br />\n" ) {
+			$content = substr( $content, 7 );
+		}
+
+		if ( substr( $content, - 4 ) == "\n<p>" ) {
+			$content = substr( $content, 0, - 4 );
+		}
+		else if ( substr( $content, - 7 ) == "<br />\n" ) {
+			$content = substr( $content, 0, - 7 );
+		}
+
+		return $content;
+
+	}
+
 	// A more forgiving version of WordPress' shortcode_atts().
-	public static function shortcode_atts( $pairs, $atts, $shortcode = '' ) {
+	public static function shortcode_atts( $pairs, $atts, $shortcode = '' ): array {
 
 		// $atts can be a string which is cast to an array. An empty string should
 		// be an empty array (not an array with an empty element as by casting).
